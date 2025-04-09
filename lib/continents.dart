3463f011d76b86968dff7277aad68ac396bae326
AibekArstanbekov <arstanbekov77@gmail.com>
2025-04-09 03:20:20 +0200

    added UI for country list

--- lib/continents.dart
+++ lib/continents.dart
@@ -1,10 +1,18 @@
+// ignore: file_names
+
 import 'package:flutter/material.dart';
+import 'package:flutter_countries_app/fuetures/app_text.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Countries by Continent')));
+    return Scaffold(
+      appBar: AppBar(
+        title: appText.header('Capitalis of the World'),
+        centerTitle: false,
+      ),
+    );
   }
 }
--- lib/fuetures/app_text.dart
+++ lib/fuetures/app_text.dart
@@ -1,67 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-enum TextType { heading, body }
-
-class AppText extends StatelessWidget {
-  const AppText({
-    super.key,
-    this.fontWeight,
-    required this.title,
-    this.maxLines,
-    this.color,
-    this.overflow,
-    this.textAlign,
-    required this.textType,
-    this.textDirection,
-    this.decoration,
-  });
-  final FontWeight? fontWeight;
-  final String title;
-  final int? maxLines;
-  final Color? color;
-  final TextOverflow? overflow;
-  final TextAlign? textAlign;
-  final TextType textType;
-  final TextDirection? textDirection;
-  final TextDecoration? decoration;
-
-  @override
-  Widget build(BuildContext context) {
-    TextStyle textStyle = getTextStyle(textType);
-    if (fontWeight != null) {
-      textStyle = textStyle.copyWith(fontWeight: fontWeight);
-    }
-    Color? textColor = color ?? textStyle.color;
-
+class AppText {
+  //бул виджет кайтаруучу метод header тибиндеги текст учун жообтуу
+  Widget header(String text) {
     return Text(
-      title,
-      overflow: overflow,
-      maxLines: maxLines,
-      textDirection: textDirection,
-      textAlign: textAlign,
-      style: textStyle.copyWith(
-        color: textColor,
-        fontWeight: fontWeight,
-        decoration: decoration,
+      text,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
       ),
     );
   }
 
-  TextStyle getTextStyle(TextType type) {
-    switch (type) {
-      case TextType.heading:
-        return const TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w900,
-          fontSize: 18,
-        );
-      case TextType.body:
-        return const TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w900,
-          fontSize: 18,
-        );
-    }
+  //бул виджет кайтаруучу метод title тибиндеги текст учун жообтуу
+  Widget title(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      ),
+    );
   }
 }
+
+AppText appText = AppText();
